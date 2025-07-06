41893088a8843061db1516fe2ef07f6ebad8daca
josselin.caer <josselin.caer@gmail.com>
2025-07-06 18:18:21 +0200

    moves footer to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Script from 'next/script';
 import './globals.css';
 
 import Header from './ui/header';
+import Footer from './ui/footer';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
         <Script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></Script>
       </body>
     </html>
