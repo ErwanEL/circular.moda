ea7523d18b80ed5973551fdff3aee569a1dac8e1
josselin.caer <josselin.caer@gmail.com>
2025-07-10 19:19:17 +0200

    remove blocs background

--- src/app/ui/cards.tsx
+++ src/app/ui/cards.tsx
@@ -51,7 +51,7 @@ export default function Cards({ products }: CardsProps) {
       : staticContent;
 
   return (
-    <section className="bg-gray-50 py-8 antialiased dark:bg-gray-900 md:py-12">
+    <section className="py-8 antialiased md:py-12">
       <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
         <div className="mb-4 items-end justify-between space-y-4 sm:flex sm:space-y-0 md:mb-8">
           <div>
