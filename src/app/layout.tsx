014eb0f8b33352a5bb92977a7e7dc3f00c072c5f
josselin.caer <josselin.caer@gmail.com>
2025-07-16 19:12:42 +0200

    fix default next favicon og naming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,37 +29,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <head>
-        <link
-          rel="icon"
-          href="./../../icon.png"
-          type="image/x-icon"
-          sizes="32x32"
-        />
-        {/* Open Graph / Facebook */}
-        <meta
-          property="og:title"
-          content="Vende tu ropa usada fácil y rápido | circular.moda Argentina"
-        />
-        <meta
-          property="og:description"
-          content="Vendé tu ropa usada fácil y rápido con circular.moda.. Enviás fotos por WhatsApp y ganás dinero sin comisiones."
-        />
-        <meta property="og:image" content="/og-image.png" />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="es_AR" />
-        {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:title"
-          content="Vende tu ropa usada fácil y rápido | circular.moda Argentina"
-        />
-        <meta
-          name="twitter:description"
-          content="Vendé tu ropa usada fácil y rápido con circular.moda.. Enviás fotos por WhatsApp y ganás dinero sin comisiones."
-        />
-        <meta name="twitter:image" content="/og-image.png" />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
