a41f72e4ddc6234c9524dc4868b4ecdf4dd92aaf
ErwanEL <leblois.erwan@hotmail.com>
2026-06-15 16:25:21 +0700

    add script to fix image orientation

--- src/app/ui/lead-magnet.tsx
+++ src/app/ui/lead-magnet.tsx
@@ -6,7 +6,7 @@ export default function LeadMagnet() {
       <div className="mx-auto max-w-screen-md px-2 py-8 text-center sm:px-4 lg:py-16">
         <div className="mx-auto max-w-2xl">
           <h2 className="mb-4 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">
-            ¡Recibí las mejores oportunidades y novedades!
+            Recibí las mejores oportunidades y novedades
           </h2>
           <p className="font-light text-gray-500 md:text-lg dark:text-gray-400">
             Suscribite para enterarte antes que nadie de nuevas prendas,
